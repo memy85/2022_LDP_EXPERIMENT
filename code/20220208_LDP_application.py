71bd8549597283c06039215cc12a6d7565e5e40b
gememy85@gmail.com <gememy85@gmail.com>
2022-02-08 08:27:26 +0000

    new stuffs

--- code/20220208_LDP_application.py
+++ code/20220208_LDP_application.py
@@ -1,4 +1,13 @@
 import pandas as pd
 import numpy as np
 from multiprocessing import Pool
+import os, sys
+from pathlib import Path
+from configparser import ConfigParser
 from 20220208_LDP_application import pdf_BLM
+
+# PATH SETTINGS
+CURDIR = Path
+parser = ConfigParser()
+parser.read('config.txt')
+
